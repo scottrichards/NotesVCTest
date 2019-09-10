8a2a98daf912a1068ca95e159e2a7c6103af0849
Scott Richards <scott.richards@glassdoor.com>
2019-09-10 15:17:11 -0700

    Initial Commit

---
+++ TestNotesVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestNotesVC
+//
+//  Created by Scott Richards on 9/10/19.
+//  Copyright © 2019 Glassdoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
